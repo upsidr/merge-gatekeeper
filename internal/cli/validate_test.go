d5170d242e8720fd38f4a8d269e31cf54806f922
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2021-08-10 21:11:07 +0900

    fix complex implementation to make then simple
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>

--- internal/cli/validate_test.go
+++ internal/cli/validate_test.go
@@ -21,63 +21,52 @@ func Test_doValidateCmd(t *testing.T) {
 		args    args
 		wantErr bool
 	}
+
 	tests := map[string]test{
-		"returns nil when the validation is success": func() test {
-			validateInvalSecond = 1
-			timeoutSecond = 2
-
-			return test{
-				args: args{
-					ctx: context.Background(),
-					cmd: &cobra.Command{},
-					vs: []validators.Validator{
-						&mock.Validator{
-							ValidateFunc: func(ctx context.Context) error { return nil },
-						},
-						&mock.Validator{
-							ValidateFunc: func(ctx context.Context) error { return nil },
-						},
+		"returns nil when the validation is success": {
+			args: args{
+				ctx: context.Background(),
+				cmd: &cobra.Command{},
+				vs: []validators.Validator{
+					&mock.Validator{
+						ValidateFunc: func(ctx context.Context) error { return nil },
+					},
+					&mock.Validator{
+						ValidateFunc: func(ctx context.Context) error { return nil },
 					},
 				},
-			}
-		}(),
-		"returns error when the validation is timeout": func() test {
-			validateInvalSecond = 1
-			timeoutSecond = 2
-
-			return test{
-				args: args{
-					ctx: context.Background(),
-					cmd: &cobra.Command{},
-					vs: []validators.Validator{
-						&mock.Validator{
-							ValidateFunc: func(ctx context.Context) error { return validators.ErrValidate },
-						},
+			},
+		},
+		"returns error when the validation is timeout": {
+			args: args{
+				ctx: context.Background(),
+				cmd: &cobra.Command{},
+				vs: []validators.Validator{
+					&mock.Validator{
+						ValidateFunc: func(ctx context.Context) error { return validators.ErrValidate },
 					},
 				},
-				wantErr: true,
-			}
-		}(),
-		"returns error when the validator returns invalid error": func() test {
-			validateInvalSecond = 1
-			timeoutSecond = 2
-
-			return test{
-				args: args{
-					ctx: context.Background(),
-					cmd: &cobra.Command{},
-					vs: []validators.Validator{
-						&mock.Validator{
-							ValidateFunc: func(ctx context.Context) error { return errors.New("err") },
-						},
+			},
+			wantErr: true,
+		},
+		"returns error when the validator returns invalid error": {
+			args: args{
+				ctx: context.Background(),
+				cmd: &cobra.Command{},
+				vs: []validators.Validator{
+					&mock.Validator{
+						ValidateFunc: func(ctx context.Context) error { return errors.New("err") },
 					},
 				},
-				wantErr: true,
-			}
-		}(),
+			},
+			wantErr: true,
+		},
 	}
 	for name, tt := range tests {
 		t.Run(name, func(t *testing.T) {
+			validateInvalSecond = 1
+			timeoutSecond = 2
+
 			if err := doValidateCmd(tt.args.ctx, tt.args.cmd, tt.args.vs...); (err != nil) != tt.wantErr {
 				t.Errorf("doValidateCmd() error = %v, wantErr %v", err, tt.wantErr)
 			}
