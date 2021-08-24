36f53fd169a2d2e4232de1ede28154c1bc1528d9
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2021-08-24 20:20:32 +0900

    use go:embed
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>

--- cmd/gatekeeper/main.go
+++ cmd/gatekeeper/main.go
@@ -1,18 +1,21 @@
 package main
 
 import (
+	_ "embed"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/upsidr/merge-gatekeeper/internal/cli"
 )
 
-const (
-	version = "v0.0.1"
+var (
+	//go:embed version.txt
+	version string
 )
 
 func main() {
-	if err := cli.Run(version, os.Args...); err != nil {
+	if err := cli.Run(strings.TrimSuffix(version, "\n"), os.Args...); err != nil {
 		fmt.Fprintf(os.Stderr, "failed to execute command: %v", err)
 		os.Exit(1)
 	}
