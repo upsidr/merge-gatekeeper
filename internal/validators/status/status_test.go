58e3a4c13bee2a5f989f07662920458689096cc7
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2021-08-24 20:05:27 +0900

    fix fails test
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>

--- internal/validators/status/status_test.go
+++ internal/validators/status/status_test.go
@@ -20,10 +20,11 @@ func Test_status_Detail(t *testing.T) {
 				},
 			},
 			want: `1 out of 2
-total job count: 2
-jobs: [job-1 job-2]
-completed job count: 1
-jobs: [job-2]`,
+
+  total job count: 2
+    jobs: [job-1 job-2]
+  completed job count: 1
+    jobs: [job-2]`,
 		},
 		"return detail when totalJobs and completeJobs is empty": {
 			s: &status{
@@ -31,10 +32,11 @@ jobs: [job-2]`,
 				completeJobs: []string{},
 			},
 			want: `0 out of 0
-total job count: 0
-jobs: []
-completed job count: 0
-jobs: []`,
+
+  total job count: 0
+    jobs: []
+  completed job count: 0
+    jobs: []`,
 		},
 	}
 
